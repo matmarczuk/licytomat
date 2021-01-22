179b1d9245aa74e1b4daa11e95a615b9e63b7766
Mateusz Marczuk <mateusz.marczuk@cybercom.com>
2021-01-22 12:11:05 +0100

    Configure s3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,10 @@
-terraform {
-    required_version = ">= 0.12"
-}
-
 provider "aws" {
     region = var.aws_region
+}
+
+terraform {
+    required_version = ">= 0.12"
+    backend "s3" {
+        key    = "terraform.tfstate"
+    }
 }
