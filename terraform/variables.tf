019816d5d174f0d9e35032267675c1e675dcc3ba
Mateusz Marczuk <marczuk@live.it>
2021-06-22 22:53:56 +0200

    Add default bucket name

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,5 +4,6 @@ variable "aws_region" {
 }
 variable "bucket_name" {
   type        = string
+  default = "matibucket-test"
   description = "The name of the bucket without the www. prefix. Normally domain_name."
 }
